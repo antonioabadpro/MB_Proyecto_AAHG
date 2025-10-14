aa6f92259729d8bce33726fb74f78bdfe2bb528b
antonioabadpro <antonioabadpro@gmail.com>
2025-10-14 12:25:34 +0200

    Javadoc del metodo 'leerFichero()' de la Clase 'Separadora'

--- src/main/java/practica/Separadora.java
+++ src/main/java/practica/Separadora.java
@@ -14,6 +14,11 @@ import java.util.Scanner;
  */
 public class Separadora
 {
+    /**
+     * Lee un fichero dado por parametro y lo muestra por pantalla
+     * @param rutaFichero Es la ruta en la que se encuentra el fichero que queremos leer
+     * @throws FileNotFoundException Lanza una excepcion en caso de que NO se encuentre la ruta del fichero introducida por parametro
+     */
     public static void leerFichero(String rutaFichero) throws FileNotFoundException
     {
         String linea = null;
