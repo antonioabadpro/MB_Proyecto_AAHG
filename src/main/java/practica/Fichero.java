8ca749b437c6e550f2504ddd64155d351f8826a6
antonioabadpro <antonioabadpro@gmail.com>
2025-10-20 21:09:18 +0200

    Creacion de la Clase 'Fichero'
    Movemos los metodos de tratamiento de Ficheros de la Clase 'Separadora' a la Clase 'Fichero''

---
+++ src/main/java/practica/Fichero.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package practica;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author AAHG-PORTATIL
+ */
+public class Fichero
+{
+    /**
+     * Lee un fichero dado por parametro y lo muestra por pantalla
+     * @param rutaFichero Es la ruta en la que se encuentra el fichero/corpus que queremos leer
+     * @throws FileNotFoundException Lanza una excepcion en caso de que NO se encuentre la ruta del fichero introducida por parametro
+     */
+    public static void leerFichero(String rutaFichero) throws FileNotFoundException
+    {
+        String linea = null;
+        Scanner sc = new Scanner(new File(rutaFichero));
+        while(sc.hasNextLine())
+        {
+            linea = sc.nextLine();
+            System.out.println(linea);
+        }
+    }
+    
+    /**
+     * Lee un fichero dado por parametro y lo muestra con el formato original por pantalla
+     * @param rutaFichero Es la ruta en la que se encuentra el fichero/corpus que queremos leer
+     * @throws FileNotFoundException Lanza una excepcion en caso de que NO se encuentre la ruta del fichero introducida por parametro
+     */
+    public static void leerFicheroSeparado(String rutaFichero) throws FileNotFoundException
+    {
+        String linea = null;
+        File f = new File(rutaFichero);
+        Scanner sc = new Scanner(f);
+        String v_linea_id[]; // Vector que contiene la linea del fichero dividida en 2 posiciones -> 0(Información NO relevante), 1(ID)
+        Integer id;
+        String texto = "";
+        boolean leyendoTexto = false;
+        
+        while(sc.hasNextLine())
+        {
+            linea = sc.nextLine(); // Leemos la linea del Corpus
+            
+            if(linea.startsWith(".I")) // Si la linea que hemos leido empieza por ".I" significa que en la pos 1, tenemos el 'id' del documento
+            {
+                leyendoTexto = false;
+                v_linea_id = linea.split("\\.I");
+                id = Integer.valueOf(v_linea_id[1].trim());
+                System.out.println("\nID: " + id);
+                System.out.println("\nTexto: \n");
+            }
+            if(linea.startsWith(".W")) // Si la linea que hemos leido empieza por ".W" significa que en la siguiente linea comienza el texto
+            {
+                leyendoTexto = true;
+            }
+            else
+            {
+                if(leyendoTexto == true) // Si la linea NO empieza por ".W" y estamos leyendo el texto, mostramos la linea correspondiente
+                {
+                    linea = linea.replaceFirst("^\\s+", ""); // Eliminamos los espacios al inicio de cada linea
+                    linea = linea.replaceAll(" {2,}", " "); // Reemplaza todos los espacios múltiples (2 o más) por un solo espacio
+                    texto = linea;
+                    System.out.println(texto);
+                }
+            }
+        }
+        sc.close();
+    }
+    
+}
--- src/main/java/practica/Separadora.java
+++ src/main/java/practica/Separadora.java
@@ -16,67 +16,6 @@ import java.util.Scanner;
 public class Separadora
 {
     /**
-     * Lee un fichero dado por parametro y lo muestra por pantalla
-     * @param rutaFichero Es la ruta en la que se encuentra el fichero/corpus que queremos leer
-     * @throws FileNotFoundException Lanza una excepcion en caso de que NO se encuentre la ruta del fichero introducida por parametro
-     */
-    public static void leerFichero(String rutaFichero) throws FileNotFoundException
-    {
-        String linea = null;
-        Scanner sc = new Scanner(new File(rutaFichero));
-        while(sc.hasNextLine())
-        {
-            linea = sc.nextLine();
-            System.out.println(linea);
-        }
-    }
-    
-    /**
-     * Lee un fichero dado por parametro y lo muestra con el formato original por pantalla
-     * @param rutaFichero Es la ruta en la que se encuentra el fichero/corpus que queremos leer
-     * @throws FileNotFoundException Lanza una excepcion en caso de que NO se encuentre la ruta del fichero introducida por parametro
-     */
-    public static void leerFicheroSeparado(String rutaFichero) throws FileNotFoundException
-    {
-        String linea = null;
-        File f = new File(rutaFichero);
-        Scanner sc = new Scanner(f);
-        String v_linea_id[]; // Vector que contiene la linea del fichero dividida en 2 posiciones -> 0(Información NO relevante), 1(ID)
-        Integer id;
-        String texto = "";
-        boolean leyendoTexto = false;
-        
-        while(sc.hasNextLine())
-        {
-            linea = sc.nextLine(); // Leemos la linea del Corpus
-            
-            if(linea.startsWith(".I")) // Si la linea que hemos leido empieza por ".I" significa que en la pos 1, tenemos el 'id' del documento
-            {
-                leyendoTexto = false;
-                v_linea_id = linea.split("\\.I");
-                id = Integer.valueOf(v_linea_id[1].trim());
-                System.out.println("\nID: " + id);
-                System.out.println("\nTexto: \n");
-            }
-            if(linea.startsWith(".W")) // Si la linea que hemos leido empieza por ".W" significa que en la siguiente linea comienza el texto
-            {
-                leyendoTexto = true;
-            }
-            else
-            {
-                if(leyendoTexto == true) // Si la linea NO empieza por ".W" y estamos leyendo el texto, mostramos la linea correspondiente
-                {
-                    linea = linea.replaceFirst("^\\s+", ""); // Eliminamos los espacios al inicio de cada linea
-                    linea = linea.replaceAll(" {2,}", " "); // Reemplaza todos los espacios múltiples (2 o más) por un solo espacio
-                    texto = linea;
-                    System.out.println(texto);
-                }
-            }
-        }
-        sc.close();
-    }
-    
-    /**
      * Extrae el identificador (.I) de cada documento del fichero/corpus
      * @param rutaFichero Es la ruta en la que se encuentra el fichero/corpus que queremos leer
      * @return Devuelve un Array con todos los identificadores del Fichero/Corpus dado por parametro
@@ -151,13 +90,6 @@ public class Separadora
                         linea = linea.replaceFirst("^\\s+", ""); // Eliminamos los espacios al inicio de cada linea
                         linea = linea.replaceAll(" {2,}", " "); // Reemplaza todos los espacios múltiples (2 o más) por un solo espacio
 
-                        // Concatenamos la línea al texto actual con un "\n" igual que en el Corpus original
-                        /*
-                        if (textoActual.isEmpty()==false)
-                        {
-                            textoActual = textoActual + "\n";
-                        }
-                        */
                         textoActual = textoActual + linea;
                     }
                 }   
